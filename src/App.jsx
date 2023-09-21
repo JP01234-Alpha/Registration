6eec2f84681957a0850a4058218091f2c7dd1273
Janpria Pal <janpriapal@gmail.com>
2023-09-21 22:27:26 +0530

    Registration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './form';
+
+function App() {
+
+  return (
+    <>
+      <Form/>
+    </>
+  )
+}
+
+export default App
---
+++ src/form.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+
+function Form() {
+    const data={name:"",email:"",password:""};
+    const [inputData,setinputData] = useState(data);
+    const [flag,setflag]= useState(false);
+    useEffect(()=> {
+        console.log("registered");
+    },[flag])
+    function handleData(e){
+        setinputData({...inputData,[e.target.name]:e.target.value})
+        console.log(inputData);
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        if(!inputData.name || !inputData.email || !inputData.password)
+        {
+            alert("all feilds are mandotory")
+        }
+        else{
+            setflag(true);
+        }
+    }
+    return (
+        <>
+        <pre>{(flag)?<h2>hello {inputData.name}, you are registered</h2>:""}</pre>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <h1>registration</h1>
+            </div>
+            <div>
+                <input type='text' placeholder="Enter your name" name="name" value={inputData.name} 
+                onChange={handleData}></input>
+            </div>
+            <div>
+                <input type='text' placeholder="Enter your email" name="email" value={inputData.email} 
+                onChange={handleData}></input>
+            </div>
+            <div>
+                <input type='text' placeholder="Enter your password" name="password" value={inputData.password} 
+                onChange={handleData}></input>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+        </>
+    )
+}
+
+export default Form;
